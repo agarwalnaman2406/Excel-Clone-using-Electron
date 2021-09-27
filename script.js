068f2b202a538d05dc0eec982d5f83bc130abb9f
Naman <agarwalnaman2406@gmail.com>
2021-09-27 21:15:06 +0530

    Solve Formula Done

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ $("document").ready(function(){
     $(".cell").on("click" , function(){
         console.log(this);
 
-        let rowId = Number($(this).attr("rid"))+1;
+        let rowId = Number($(this).attr("rid"));
         let colId = Number($(this).attr("cid"));
-        let cellAddress = String.fromCharCode(65+colId) + rowId;
-        console.log("rowId " , rowId);
-        console.log("colId " , colId);
-        console.log(cellAddress);
+        let cellAddress = String.fromCharCode(65+colId) + (rowId+1);
+        // console.log("rowId " , rowId);
+        // console.log("colId " , colId);
+        // console.log(cellAddress);
         $("#address").val(cellAddress);
+        let cellObject = db[rowId][colId];
+        $("#formula").val(cellObject.formula);
     })
 
     $(".cell").on("blur" , function(){
@@ -24,10 +26,69 @@ $("document").ready(function(){
         let rowId = Number($(this).attr("rid"));
         let colId = Number($(this).attr("cid"));
         let cellObject = db[rowId][colId];
-        cellObject.value = value;
-        console.log(cellObject);
-        console.log(db);
+        if(cellObject.value != value){
+            cellObject.value = value;
+            console.log(cellObject);
+            console.log(db);
+        }
+        
     })
+
+    $("#formula").on("blur", function(){
+
+        let formula = $(this).val();
+        // console.log(formula);
+        let value = solveFormula(formula);
+        // Db Update
+        let address = $("#address").val();
+        let {rowId, colId} = getRowIdAndColID(address);
+        let cellObject = db[rowId][colId];
+        if(cellObject.formula != formula){
+            cellObject.value = value;
+            cellObject.formula = formula;
+        }
+        
+        // UI update
+        $(lsc).text(value);
+
+    })
+
+    function solveFormula(formula){
+        // formula = ( A1 + A2 )
+        let fComponents = formula.split(" ");
+        // ["(" , "A1", "+", "A2, ")]
+        for(let i=0;i<fComponents.length;i++){
+            let fComp = fComponents[i];
+            // fcomp = A1;
+            let cellname = fComp[0];
+            if(cellname >= 'A' && cellname <= 'Z'){
+                // comp = A1
+                // A1 -> rowId and ColId
+                let {rowId, colId} = getRowIdAndColID(fComp);
+                let cellObject = db[rowId][colId];
+                // {
+                //     name: "A1",
+                //     value: "10",
+                //     formula : ""
+                // }
+                let value = cellObject.value + "";
+                formula = formula.replace(fComp, value);
+            }
+        }
+
+        let value = eval(formula);
+        return value;
+    }
+
+    // utility function
+    function getRowIdAndColID(address){
+        // address = "A1", "B2", "D5";
+        let colId = address.charCodeAt(0) - 65;
+        let rowId = Number(address.substring(1))-1;
+        return {rowId : rowId,
+                colId : colId}
+
+    }
     
     function init(){
         // db = 26 * 100
